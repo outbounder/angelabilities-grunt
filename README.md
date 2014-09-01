690b3d46ff27f74fddf8479c9d31160b6f182565
Boris Filipov <obiwonn@gmail.com>
2014-09-01 14:31:35 +0300

    autoload grunt tasks and npm modules
    * updated readme
    * improved autoloading mechanism
    * added support for Gruntfile.js

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # angelabilities-grunt
 
-A quick 'hack' for giving organic-angel grunt abilities. It is based on grunt 0.4 and autoloads all grunt modules from `devDependencies` found in package.json. No Gruntfile is required or used.
+A quick 'hack' for giving organic-angel grunt abilities. It is based on grunt 0.4 and autoloads all grunt modules found in node_modules. No Gruntfile is required.
+
+## setup
+
+    $ cd myProject
+    $ npm install angelabilities-grunt
+
+### optionally use `Gruntfile.js`
+
+    $ cd myProject
+    $ echo "Gruntfile.js" >> ./dna/gruntfile.json
+    $ vim Gruntfile.js
+
+### optionally use grunt configuration
+
+    $ cd myProject
+    $ echo "{... gruntConfiguration ...}" >> ./dna/grunt.json
+
+## use
+
+    $ cd myProject
+    $ angel grunt -h 
+    $ angel grunt <grunt task>
+
+### add your own local grunt tasks without Gruntfile.js
+
+    $ vim scripts/grunt/myTask.js
--- index.js
+++ index.js
@@ -19,30 +19,34 @@ module.exports = function(angel, done) {
     var result = extractGruntRunTasksAndOptions(angel.cmdData[1].split(" "))
     angel.grunt.tasks(result.tasks, result.options)
   })
-  .example("$ angel grunt <task ?>")
+  .example("$ angel grunt -h")
 
-  var packagejson = path.join(process.cwd(), "package.json")
+  var node_modules = path.join(process.cwd(), "node_modules")
+  var local_grunt_scripts = path.join(process.cwd(), "scripts", "grunt")
 
-  fs.readFile(packagejson, function(err, content){
-    if(err) return console.warn("package.json not found, can't load grunt tasks")
+  fs.readdir(node_modules, function(err, results){
+    if(err) return console.warn(err, " error found during node_modules scanning, can't load grunt tasks")
     try {
-      var json = JSON.parse(content)
-      // hack to avoid loading a Gruntfile
-      // You can skip this and just use a Gruntfile instead
-      angel.grunt.task.init = function() {};
+      // either use Gruntfile or 'hack' it.
+      if(angel.dna.gruntfile)
+        angel.grunt.task.init = require(path.join(process.cwd(), angel.dna.gruntfile))
+      else
+        angel.grunt.task.init = function() {};
 
       // Init config
       angel.grunt.initConfig(angel.dna.grunt || {});
 
-      // Load tasks from npm
-      for(var key in json.devDependencies) {
-        if(key.indexOf("grunt-") == 0) {
-          angel.grunt.loadNpmTasks(key)
-        }
-      }
+      // Load tasks
+      results.forEach(function(r){
+        if(r.indexOf("grunt-") == 0)
+          angel.grunt.loadNpmTasks(r)
+      })
+
+      angel.grunt.loadTasks(local_grunt_scripts)
+
       done()
     } catch(err){
-      console.error("failed to parse package.json", err)
+      console.error("failed to initialize angel grunt ability", err)
     }
   })
 }
